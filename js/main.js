da7e02233767cc0fb91f12081d6585045c2b345e
Jorge Goris <J_goris@live.com>
2020-07-07 15:43:37 -0400

    Added scroll functionality with jQuery to navigation and links, Made slight changes to some media queries.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,19 @@ $(function() {
     });
 })
 
+//Scrolling Effect
+$(document).ready(function(){
+    let scrollLink = $('.scroll');
+
+    //smooth scrolling
+    scrollLink.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    })
+})
+
 //Form Validation
 $('.submit-btn').click(function(e){
     e.preventDefault();
