f87c113df6dd01edf406d5278d62ebceed7bfb79
Jorge Goris <J_goris@live.com>
2020-07-20 17:28:10 -0400

    Contact form succesfully working with Formspree.io

--- js/main.js
+++ js/main.js
@@ -24,39 +24,38 @@ $(document).ready(function(){
     })
 })
 
-//Form Validation
-$('.submit-btn').click(function(e){
-    e.preventDefault();
-    console.log('Submit clicked!')
+// //Form Validation
+// $('.submit-btn').click(function(e){
+//     e.preventDefault();
+//     var name = $('.name-field').val()
+//     var email = $('.email-field').val()
+//     var message = $('.msg-field').val()
+//     var errormsg = $('.error-msg')
+//     errormsg.empty()
 
-    var name = $('.name-field').val()
-    var email = $('.email-field').val()
-    var message = $('.msg-field').val()
-    var errormsg = $('.error-msg')
-    errormsg.empty()
 
-    //Name Validation
-    if(name.length > 2){
-        errormsg.append('Name is valid ')
-    }
-    else {
-        errormsg.append('Name is not valid! Needs more than 2 characters!')
-    }
+//     //Name Validation
+//     if(name.length > 2){
+//         errormsg.append('Name is valid ')
+//     }
+//     else {
+//         errormsg.append('Name is not valid! Needs more than 2 characters! ')
+//     }
 
-    //Email Validation
-    if(email.length > 5 && email.includes('@') && email.includes('.')){
-        errormsg.append('Email is valid ')
-    }
-    else {
-        errormsg.append('Email is not valid! Must be greater than 5 characters, include an @ sign and a `.` ')
-    }
+//     //Email Validation
+//     if(email.length > 5 && email.includes('@') && email.includes('.')){
+//         errormsg.append('Email is valid ')
+//     }
+//     else {
+//         errormsg.append('Email is not valid! Must be greater than 5 characters, include an @ sign and a `.`  ')
+//     }
 
-    //Message Validation
-    if(message.length > 10){
-        errormsg.append('Message is Valid ')
-    }
-    else {
-        errormsg.append('Message is not valid! Must be greater than 20 characters.')
-    }
+//     //Message Validation
+//     if(message.length > 10){
+//         errormsg.append('Message is Valid ')
+//     }
+//     else {
+//         errormsg.append('Message is not valid! Must be greater than 20 characters.')
+//     }
 
-})
+// })
